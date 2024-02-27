b59732fc5fdfc9e416a6db07ab5aa243b3e9eccc
Tomasz Szczepanik <szczepaniktom@gmail.com>
2024-02-27 12:44:39 +0100

    Removed steps for writing functions

--- src/js/script.js
+++ src/js/script.js
@@ -67,32 +67,22 @@ const select = {
 
     renderInMenu(){
       const thisProduct = this;
-      /* generate HTML based on template */
       const generatedHTML = templates.menuProduct(thisProduct.data);
-      /* create element using utlis.createElementFromHTML */
       thisProduct.element = utils.createDOMFromHTML(generatedHTML);
-      /* find menu container */
       const menuContainer = document.querySelector(select.containerOf.menu);
-      /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
     }
 
     initAccordion(){
       const thisProduct = this;
-      /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable)
-
-      /* START: add event listener to clickable trigger on event click */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       clickableTrigger.addEventListener('click', function(event) {
-        /* prevent default action for event */
         event.preventDefault()
-        /* find active product (product that has active class) */
         const activeProduct = thisProduct.element.querySelector('.product.active')
-        /* if there is active product and it's not thisProduct.element, remove class active from it */
         if (activeProduct && activeProduct !== thisProduct.element) {
           activeProduct.classList.remove('active');
         }
-        /* toggle active class on thisProduct.element */
+
         thisProduct.element.classList.toggle('active');
       });
 
